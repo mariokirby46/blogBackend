f80c42be28a898ab27f214f099b519ee63925b42
mariokirby46 <hillmanhung@yahoo.com>
2021-06-30 15:15:38 +0800

    updates

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv/config')
 const express = require('express')
 const mongoose = require('mongoose')
-
+const cors = require('cors')
 
 const router = require('./router')
 
@@ -11,7 +11,9 @@ const app = express()
 mongoose.connect(process.env.db, {useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify:false,useCreateIndex:true})
     .catch((err)=>{throw new Error('error connecting to db ' +err)});
 
+app.use(cors())
 app.use(express.urlencoded({extended:false}))
+app.use(express.json())
 app.use('/',router)
 
 
